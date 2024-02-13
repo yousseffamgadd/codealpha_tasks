a6060aa4932e48bd637ceddada1114c77845e830
youssef amgad <55314119+yousseffamgadd@users.noreply.github.com>
2024-02-13 23:12:40 +0200

    Update TASK3 (securecodereciew).bat

--- TASK3 (securecodereciew).bat
+++ TASK3 (securecodereciew).bat
@@ -1,13 +1,17 @@
 @echo off
-echo this batch will help you see vulnerabilities of any python application
+echo This batch script will help identify vulnerabilities in any Python application.
 
 REM Check if an argument is provided
-if "%1"=="" (
-    echo Usage: %0 directory_path
+if "%~1"=="" (
+    echo Usage: %0 [directory_path_or_file_path]
     exit /b 1
 ) else (
-    REM Run Bandit on Python files within the specified directory
-    bandit -r %1
+    REM Run Bandit on Python files within the specified directory or directly on the provided Python file
+    if exist %~1 (
+        bandit -r %~1
+    ) else (
+        bandit %~1
+    )
 )
 
 
